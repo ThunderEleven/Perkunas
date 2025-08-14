a62e31deb69f3c0181de9f80da1086967d2d006a
DMDZ99 <ldhdo430@naver.com>
2025-08-14 15:13:18 +0900

    프리팹

--- Perkunas/Assets/Scripts/Item/ItemObject.cs
+++ Perkunas/Assets/Scripts/Item/ItemObject.cs
@@ -1,15 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 
 public interface IInteractable  // 상호작용 인터페이스
 {
+    public string GetInteractPrompt();
     public void OnInteract();
 }
 
 public class ItemObject : MonoBehaviour, IInteractable
 {
+    public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
     public void OnInteract()
     {
         // 자원 아이템 데이터 저장

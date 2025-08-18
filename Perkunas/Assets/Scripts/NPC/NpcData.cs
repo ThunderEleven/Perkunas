70c05161adcbe9a1ebfda4b8bd2be75222d613f1
jyj8943 <jyj8943@gmail.com>
2025-08-18 16:55:17 +0900

    [feat] 모노싱글톤 추가, npc 데이터 매니저, npc 데이터 스크립트 분리

--- Perkunas/Assets/Scripts/NPC/NpcData.cs
+++ Perkunas/Assets/Scripts/NPC/NpcData.cs
@@ -3,57 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class DialogueEntry
-{
-    public int id;
-    public string npcName;
-    public string text;
-}
-
-// public class NpcData : MonoBehaviour
-// {
-//     public string npcName;
-//     private List<Dictionary<string, object>> dialogue;
-//     
-//     private void Start()
-//     {
-//         dialogue = CSVReader.Read("DialogueData");
-//
-//         for (int i = 0; i < dialogue.Count; i++)
-//         {
-//             Debug.Log("대화 번호: " + dialogue[i]["대화 번호"]);
-//             Debug.Log("NPC 이름: " + dialogue[i]["npc 이름"]);
-//             Debug.Log("대화 내용: " + dialogue[i]["대화 내용"]);
-//         }
-//     }
-// }
-
-// npc 데이터에 CSV 파일에서 가져온 대화 데이터가 잘 들어가는지 확인하는 코드
 public class NpcData : MonoBehaviour
 {
     public string npcName;
-
-    [SerializeField] private List<DialogueEntry> dialogues; // Inspector에서 보임
-
+    [SerializeField] private List<DialogueEntry> dialogue;
+    
     private void Start()
     {
-        var rawData = CSVReader.Read("DialogueData");
-        dialogues = new List<DialogueEntry>();
-
-        foreach (var row in rawData)
+        // 해당 npc의 이름을 가진 대화 데이터만 가져오도록 구현
+        foreach (var data in NpcDataManager.Instance.Dialogues)
         {
-            DialogueEntry entry = new DialogueEntry();
-            entry.id = (int)row["대화 번호"];
-            entry.npcName = row["npc 이름"].ToString();
-            entry.text = row["대화 내용"].ToString();
-            dialogues.Add(entry);
-        }
-
-        // 확인용
-        foreach (var d in dialogues)
-        {
-            Debug.Log($"[{d.id}] {d.npcName}: {d.text}");
+            if (data.npcName == this.npcName)
+            {
+                dialogue.Add(data);
+            }
         }
     }
 }

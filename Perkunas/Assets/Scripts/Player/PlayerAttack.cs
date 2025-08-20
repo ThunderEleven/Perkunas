baf21010d7b05a9b0a3f9cee83e91cd648b839e7
lhr4426 <lhr4426@naver.com>
2025-08-20 20:23:50 +0900

    [fix] 디버그 로그 삭제

--- Perkunas/Assets/Scripts/Player/PlayerAttack.cs
+++ Perkunas/Assets/Scripts/Player/PlayerAttack.cs
@@ -5,7 +5,7 @@ public class PlayerAttack : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("충돌함" + collision.gameObject.name);
+        // Debug.Log("충돌함" + collision.gameObject.name);
         // 부딪힌 오브젝트의 태그가 "Monster"인지 확인
         if (collision.gameObject.CompareTag("Monster"))
         {

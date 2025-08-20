c979cf0e35f1fd9d4698739096f4b828fcd8ec49
kiub-15 <kmk923140@gmail.com>
2025-08-20 16:51:37 +0900

    [feat] 공격 막대기 구현

--- Perkunas/Assets/Scripts/Manager/MonoSingleton.cs
+++ Perkunas/Assets/Scripts/Manager/MonoSingleton.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
-
 public abstract class MonoSingleton<T> : MonoBehaviour where T : Component
 {
     protected static T _instance;

81e1ecf814080ae576f5b9e8624ac0c75b384634
jyj8943 <jyj8943@gmail.com>
2025-08-21 10:28:42 +0900

    몬스터 데이터

--- Perkunas/Assets/Scripts/Monster/Individual Data/MonsterData.cs
+++ Perkunas/Assets/Scripts/Monster/Individual Data/MonsterData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 [CreateAssetMenu(fileName = "New Monster", menuName = "Monster")]
 public class MonsterData : ScriptableObject
 {

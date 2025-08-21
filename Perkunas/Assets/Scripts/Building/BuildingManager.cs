6e5833612b01405f63ed3c55a4f60fb12e4e85a1
rootinge <jhpark5129@gmail.com>
2025-08-21 11:04:14 +0900

    [feat] 건축 완 (합쳐서 빌드 해봐야 함)

--- Perkunas/Assets/Scripts/Building/BuildingManager.cs
+++ Perkunas/Assets/Scripts/Building/BuildingManager.cs
@@ -12,16 +12,11 @@ public class BuildingManager : MonoBehaviour
     [SerializeField] private int distanceFromPlayer = 5; // 플레이어로부터의 거리
     [SerializeField] private float gizmoSize = 0.2f; // 기즈모 크기
 
-    void Start()
-    {
+    [SerializeField] private Material transparentMat; // 투명 재질
+    [SerializeField] private LayerMask buildLayer; // 빌드 레이어
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    GameObject visualisedObjectType; // 시각화된 오브젝트 타입 GameObject -> item 변경
+    Transform visualisedObject; // 시각화된 오브젝트 Transform
 
     void OnDrawGizmos()
     {
@@ -87,6 +82,11 @@ public class BuildingManager : MonoBehaviour
         //    }
         //}
 
+        if(visualisedObject == null || visualisedObjectType != obj)
+        {
+            StartVisualisingObject(obj); // 오브젝트 시각화 시작
+        }
+
         VisualiseObject(pos, rotY, obj); // 오브젝트 시각화
     }
 
@@ -94,6 +94,46 @@ public class BuildingManager : MonoBehaviour
     private void VisualiseObject(Vector3 pos, float rotY, GameObject obj)
     {
         bool isOccupied = false;
+
+        Vector3 direction = (pos - transform.position).normalized; // 플레이어와 오브젝트 위치 간의 방향 벡터
+        visualisedObject.position = pos + direction * -0.01f;
+        visualisedObject.rotation = Quaternion.Euler(0, rotY, 0); // 시각화된 오브젝트 회전 설정
+
+
+        visualisedObject.position = pos; // 시각화된 오브젝트 위치 설정
+        visualisedObject.rotation = Quaternion.Euler(0, rotY, 0); // 시각화된 오브젝트 회전 설정
+
+        Collider[] colliders = visualisedObject.GetComponentsInChildren<Collider>(); // 시각화된 오브젝트의 모든 콜라이더 가져오기
+        foreach (Collider collider in colliders)
+        {
+            bool breakBothLoops = false; // 두 루프를 모두 중단할지 여부
+
+            collider.isTrigger = true; // 모든 콜라이더를 트리거로 설정
+
+            RaycastHit[] hits = Physics.BoxCastAll(collider.bounds.center, new Vector3(collider.bounds.size.x * 0.4f, collider.bounds.size.y * 0.4f,
+collider.bounds.size.z * 0.4f), Vector3.up, Quaternion.identity, 1, buildLayer); // 박스 캐스트를 사용하여 충돌 검사
+
+            foreach (RaycastHit hit in hits)
+            {
+                // BuiltObject가 있는지 확인
+               /* if (hit.collider != null && hit.collider.GetComponentInParent<BuiltObject>() != null &&
+    !obj.ignoreObjects.Contains(hit.collider.GetComponentInParent<BuiltObject>().objectType))*/
+                {
+                    isOccupied = true;
+                    breakBothLoops = true;
+                    break;
+                }
+            }
+
+            if(breakBothLoops) // 두 루프를 모두 중단해야 하는 경우
+            {
+                break; // 외부 루프 중단
+            }
+        }
+
+        Color color = isOccupied ? new Color(0.7f, 0.3f, 0.3f, 0.5f) : new Color(0.3f, 0.7f, 0.3f, 0.5f); // 충돌 여부에 따라 색상 설정
+        transparentMat.color = color; // 투명 재질의 색상 설정
+
         if (Input.GetMouseButtonDown(0) && !isOccupied) // 마우스 왼쪽 버튼 클릭 시
         {
             BuildObject(pos, rotY, obj); // 오브젝트 배치
@@ -101,15 +141,44 @@ public class BuildingManager : MonoBehaviour
     }
 
     // 오브젝트 시각화 시작
-    private void StartVisualisingObject(Vector3 pos, float rotY, GameObject obj)
+    private void StartVisualisingObject(GameObject obj)
     {
+        visualisedObjectType = obj; // 시각화된 오브젝트 타입 설정
 
+        if(visualisedObject != null)
+        {
+            Destroy(visualisedObject.gameObject); // 기존 시각화된 오브젝트 제거
+        }
+
+        visualisedObject = Instantiate(visualisedObjectType).transform; // 새로운 시각화된 오브젝트 생성 visualisedObjectType.prefab
+        visualisedObject.localScale = new Vector3(cellWidth, cellHeight, cellWidth); // 시각화된 오브젝트 크기 조정
+
+        MeshRenderer renderer = visualisedObject.GetComponentInChildren<MeshRenderer>();
+        Material[] materials = new Material[renderer.materials.Length];
+
+        for(int i = 0; i < materials.Length; i++)
+        {
+            materials[i] = transparentMat;
+        }
+
+        renderer.materials = materials; // 시각화된 오브젝트의 재질을 투명 재질로 설정
+
+        visualisedObject.gameObject.layer = 2;
+
+        foreach (Transform child in visualisedObject)
+        {
+            child.gameObject.layer = 21; // 자식 오브젝트의 레이어를 3으로 설정
+        }
     }
 
     // 오브젝트 시각화 종료
     public void EndVisualisingObject()
     {
+        if(visualisedObject != null)
+        {
+            Destroy(visualisedObject.gameObject); // 시각화된 오브젝트 제거
 
+        }
     }
 
 
@@ -117,5 +186,34 @@ public class BuildingManager : MonoBehaviour
     {
         GameObject newObj = Instantiate(obj, pos, Quaternion.Euler(0, rotY, 0)); // 오브젝트 생성
         newObj.transform.localScale = new Vector3(cellWidth, cellHeight, cellWidth); // 오브젝트 크기 조정
+
+
+        int layerIndex = Mathf.FloorToInt(Mathf.Log(buildLayer, 2)); // 레이어 인덱스 계산
+
+        foreach (Transform child in newObj.transform)
+        {
+            child.gameObject.layer = layerIndex; // 자식 오브젝트의 레이어를 설정
+        }
+
+        newObj.layer = layerIndex; // 오브젝트의 레이어를 설정
+
+        BuiltObject builtObject = newObj.GetComponent<BuiltObject>(); // BuiltObject 컴포넌트 가져오기
+        // builtObject.objectType = obj;
     }
 }
+
+
+
+/*
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item")]
+public class ItemSO : ScriptableObject
+{
+    public string name;
+    public Sprite icon;
+    public GameObject prefab;
+    public int stackMax;
+    public bool placable;
+    public bool snapToGridEdge;
+    public List<ItemSO> ignoreObjects;
+}
+*/
---
+++ Perkunas/Assets/Scripts/Item/BuiltObject.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuiltObject : MonoBehaviour
+{
+    public ItemData objectType;
+}
